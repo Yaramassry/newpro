28af1419f30d6558f62768f6d6bc96b74ef506db
YaraMassry <yaramassry1995@gmail.com>
2020-10-26 16:11:08 +0300

    ms

--- src/album.js
+++ src/album.js
@@ -1,14 +1,56 @@
 import React, { Component } from "react";
+import Nav from './nav';
+import './footer.css';
+
+import axios from 'axios';
 
 class album extends React.Component{
-    const axios = require('axios');
+  constructor(){
+    super();
+    this.state = {
+      albums: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/photos`)
+      .then(res => {
+        const photos = res.data;
+        this.setState({ albums: photos });
+      })
+  }
 
 render(){
-    return(
-        <div>
-            album
-            </div>
-    )
+  return(
+
+<div className="footer-home">
+
+<div className="container">
+
+
+ <div className="row">
+
+
+       {this.state.albums.map(album => (
+          <div key={album.id} className="col-lg-3 col-md-4 col-sm-6 col-12">
+          <div  >
+               <a href={album.url}><img className="img" src={album.thumbnailUrl}  /></a>
+             <h3>{album.title}</h3>
+             <br/> <br/> <br/>
+          </div>
+
+
+         </div>
+          ))}
+
+        </div>
+
+
+
+
+        </div>
+</div>
+  )
 }
 
 
--- src/nav.js
+++ src/nav.js
@@ -9,38 +9,38 @@ class nav extends  React.Component{
     render(){
         return(
         <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-dark bg-dark">
-          <a class="navbar-brand mb-0 h1" href="#">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light navbar-dark bg-dark">
+          <a className="navbar-brand mb-0 h1" href="#">
           <img src={require('./photo/lacasa.jpg')} width="30" height="30" alt="" loading="lazy"/>
            MyMov
           </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse"
-         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+        <button className="navbar-toggler" type="button" data-toggle="collapse"
+         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
          aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-      
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Login</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Login</a>
             </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Types of Films
               </a>
               <div className="dropdown-menu" aria-labelledby="navbarDropdown">
                             <a className="dropdown-item" href="#">Comedy</a>
-                                    
+
                                 <ul>
                                     <li className="nav-item dropdown"><a href="#">Dump and Dumper</a></li>
                                     <li className="nav-item dropdown"><a href="#">Ted</a></li>
                                     <li className="nav-item dropdown"><a href="#">Plus One</a></li>
                                 </ul>
-                        
+
 
                     <a className="dropdown-item" href="#">Action</a>
                                     <ul>
@@ -48,7 +48,7 @@ class nav extends  React.Component{
                                     <li className="nav-item dropdown"><a href="#">Game of thrones</a></li>
                                     <li className="nav-item dropdown"><a href="#">Lacaca</a></li>
                                 </ul>
-                                        
+
                             <a className="dropdown-item" href="#">Romantic</a>
                                         <ul>
                                             <li className="nav-item dropdown"><a href="#">Titanic</a></li>
@@ -59,27 +59,27 @@ class nav extends  React.Component{
                                             <li className="nav-item dropdown"><a href="#">MyDad</a></li>
                                         </ul>
                     </div>
-                 
-                  
-            
-           
+
+
+
+
             </li>
-           
+
           </ul>
-          
+
       {/* <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
           </form>
-          
+
           */}
         </div>
-      </nav>  
+      </nav>
 
 
-       
+
       </div>
-      
+
         )
     }
 }
