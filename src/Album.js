55df20a2d12af6012052b2a0ad7bd22cc09deae2
YaraMassry <yaramassry1995@gmail.com>
2020-10-26 23:33:18 +0300

    msg

---
+++ src/Album.js
@@ -0,0 +1,18 @@
+
+import React, { Component } from "react";
+
+class Album extends React.Component {
+  render() {
+    return (
+     <div className="card col-lg-3 col-md-4 col-sm-6 col-12">
+      <a href={this.props.url}><img className="card-img-top" src={this.props.thumbnailUrl}  /></a>
+      <div className="card-body">
+        <h5 className="card-title">{this.props.title}</h5>
+      </div>
+    </div>
+    )
+  }
+}
+
+export default Album;
+
